e558f0d20f1f1958fbf61962956a66958fec8401
SuhyeonB <peridot489@gmail.com>
2024-07-05 05:20:25 +0900

    client initial

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,10 +12,10 @@ const Header = () => {
                 <div id='nav'>
                     <ul>
                         <li><Link to='/'>HOME</Link></li>
-                        <li><Link to='/'>MEMBER</Link></li>
-                        <li><Link to='/'>RESEARCH</Link></li>
-                        <li><Link to='/'>CONTACT</Link></li>
-                        <li><Link to='/'>BOARD</Link></li>
+                        <li><Link to='/member'>MEMBER</Link></li>
+                        <li><Link to='/research'>RESEARCH</Link></li>
+                        <li><Link to='/contact'>CONTACT</Link></li>
+                        <li><Link to='/board'>BOARD</Link></li>
                     </ul>
                 </div>
             </nav>
